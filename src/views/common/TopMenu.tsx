11adca00ef815a6888fb145db51ed532bea09cfd
Κwtsas kalofonos <konkalof1@gmail.com>
2022-05-24 01:10:04 +0300

    Landing Page

---
+++ src/views/common/TopMenu.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "../../assets/Frame.png";
+
+const TopMenu: React.FunctionComponent = () =>{
+
+    return(
+        <nav className="navbar" role="navigation" aria-label="main-navigation">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="https://bulma.io">
+                <img src={logo} width="212" height="28"/>
+                </a>
+
+                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" className="navbar-menu">
+                <div className="navbar-end">
+                    <a className="navbar-item">Home</a>
+                    <a className="navbar-item">About Us</a>
+                    <a className="navbar-item">Price</a>
+                    <a className="navbar-item">Blog</a>
+                    <a className="navbar-item">Contact Us</a>
+                    <div className="navbar-item">
+                    <div className="buttons">
+                    <a className="button is-outlined" style={{"border":"1px solid #7E6FD8","color":"#7E6FD8"}}>Log in</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </nav>
+    )
+};
+
+export default TopMenu;
