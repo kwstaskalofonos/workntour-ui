05031ddd2bd252fcf40b8a99ca0d5912b3be0f6d
Κwtsas kalofonos <konkalof1@gmail.com>
2022-06-23 23:18:10 +0300

    Fixes

--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -30,8 +30,8 @@ export const LandingPage: React.FunctionComponent = () =>{
                         <div className="column is-narrow-desktop has-text-center">
                             <p className="is-size-2 has-text-weight-bold mb-4" style={{"color":"#6455BB"}}>Work. Travel. Connect.</p>
                             <p className="is-size-4 has-text-weight-medium" style={{"color":"#03D0BC"}}>Workntour promotes development through travel!</p>
-                            <p className="is-size-5 has-text-weight-normal" style={{"color":"#8B9389","opacity":"0.7"}}>The travelers work for a fiew hours a day in exchange for food</p>
-                            <p className="is-size-5 has-text-weight-normal" style={{"color":"#8B9389","opacity":"0.7"}}>and accomodation, while the hosts make use of their skills and</p>
+                            <p className="is-size-5 has-text-weight-normal" style={{"color":"#8B9389","opacity":"0.7"}}>The travelers offer to work for a few hours a day in exchange for food</p>
+                            <p className="is-size-5 has-text-weight-normal" style={{"color":"#8B9389","opacity":"0.7"}}>and accommodation, while the hosts make use of their skills and</p>
                             <p className="is-size-5 has-text-weight-normal mb-6" style={{"color":"#8B9389","opacity":"0.7"}}>time during their stay.</p>
                             <button className="button is-info has-text-dark" onClick={()=>scrollToSection()}>Learn More</button>
                         </div>
