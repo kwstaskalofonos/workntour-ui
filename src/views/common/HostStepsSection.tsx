a01e20f18d145dd3f3553abaa15c7fab36078590
Κwtsas kalofonos <konkalof1@gmail.com>
2022-06-23 23:08:43 +0300

    Changed steps from images to code.

--- src/views/common/HostStepsSection.tsx
+++ src/views/common/HostStepsSection.tsx
@@ -7,14 +7,6 @@ import joinTheCom from "@src/assets/hosts/joinTheCommunity.png";
 import liveLikeALocal from "@src/assets/hosts/liveLikeALocal.png";
 // @ts-ignore
 import review from "@src/assets/hosts/review.png";
-// @ts-ignore
-import one from "@src/assets/hosts/Group1.png";
-// @ts-ignore
-import two from "@src/assets/hosts/Group2.png";
-// @ts-ignore
-import three from "@src/assets/hosts/Group3.png";
-// @ts-ignore
-import four from "@src/assets/hosts/Group4.png";
 
 const HostStepsSection: React.FunctionComponent = () =>{
 
@@ -22,23 +14,51 @@ const HostStepsSection: React.FunctionComponent = () =>{
         <React.Fragment>
             {/* STEP 1 */}
             <div className="columns is-vcentered is-centered">
-                <div className="column is-narrow has-text-right"><img src={one} width={210} height={122}/></div>
+                <div className="column is-narrow is-flex is-align-content-baseline">
+                        <p className={"is-italic has-text-info has-text-weight-bold"} style={{fontSize:"76px"}}>1.</p>
+                        <div className={"ml-4 pt-5"}>
+                            <p className={"has-text-primary has-text-weight-bold"} style={{fontSize:"18px"}}>Join the community</p>
+                            <p className={"has-text-weight-semibold"} style={{fontSize:"14px"}}>Describe the opportunity <br/>you offer</p>
+                        </div>
+                </div>
+                <div className={"column is-1"}></div>
                 <div className="column is-narrow has-text-left"><img src={joinTheCom} width={271} height={271}/></div>
             </div>
             {/* STEP 2 */}
             <div className="columns is-vcentered is-centered">
                 <div className="column is-narrow has-text-right"><img src={conWithHosts} width={271} height={271}/></div>
-                <div className="column is-narrow has-text-left"><img src={two} width={210} height={122}/></div>
+                <div className={"column is-1"}></div>
+                <div className="column is-narrow is-flex is-align-content-baseline">
+                    <p className={"is-italic has-text-info has-text-weight-bold"} style={{fontSize:"76px"}}>2.</p>
+                    <div className={"ml-4 py-6"}>
+                        <p className={"has-text-primary has-text-weight-bold"} style={{fontSize:"18px"}}>Connect with Travelers</p>
+                        <p className={"has-text-weight-semibold"} style={{fontSize:"14px"}}>Travelers can directly contact you</p>
+                    </div>
+                </div>
             </div>
             {/* STEP 3 */}
             <div className="columns is-vcentered is-centered">
-                <div className="column is-narrow has-text-right"><img src={three} width={210} height={122}/></div>
+                <div className="column is-narrow is-flex is-align-content-baseline">
+                    <p className={"is-italic has-text-info has-text-weight-bold"} style={{fontSize:"76px"}}>3.</p>
+                    <div className={"ml-4 py-6"}>
+                        <p className={"has-text-primary has-text-weight-bold"} style={{fontSize:"18px"}}>Share your culture</p>
+                        <p className={"has-text-weight-semibold"} style={{fontSize:"14px"}}>Accept the offer</p>
+                    </div>
+                </div>
+                <div className={"column is-1"}></div>
                 <div className="column is-narrow has-text-left"><img src={liveLikeALocal} width={271} height={271}/></div>
             </div>
             {/* STEP 4 */}
             <div className="columns is-vcentered is-centered">
                 <div className="column is-narrow has-text-right"><img src={review} width={271} height={271}/></div>
-                <div className="column is-narrow has-text-left"><img src={four} width={210} height={122}/></div>
+                <div className={"column is-1"}></div>
+                <div className="column is-narrow is-flex is-align-content-baseline">
+                    <p className={"is-italic has-text-info has-text-weight-bold"} style={{fontSize:"76px"}}>4.</p>
+                    <div className={"ml-4 py-6"}>
+                        <p className={"has-text-primary has-text-weight-bold"} style={{fontSize:"18px"}}>Review</p>
+                        <p className={"has-text-weight-semibold"} style={{fontSize:"14px"}}>Give your feedback</p>
+                    </div>
+                </div>
             </div>
         </React.Fragment>
     )
--- src/views/common/TravelerStepsSection.tsx
+++ src/views/common/TravelerStepsSection.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 // @ts-ignore
-import one from "@src/assets/traveler/1.png";
-// @ts-ignore
 import bePartOf from "@src/assets/traveler/bePartOf.png";
-// @ts-ignore
-import two from "@src/assets/traveler/2.png";
 // @ts-ignore
 import findYourPref from "@src/assets/traveler/findYourPref.png";
 // @ts-ignore
-import three from "@src/assets/traveler/3.png";
-// @ts-ignore
 import bookYourStay from "@src/assets/traveler/bookYourStay.png";
-// @ts-ignore
-import four from "@src/assets/traveler/4.png";
 // @ts-ignore
 import review from "@src/assets/traveler/review.png";
 
@@ -23,23 +15,52 @@ const TravelerStepsSection: React.FunctionComponent = () =>{
             <React.Fragment>
                 {/* STEP 1 */}
                 <div className="columns is-vcentered is-centered">
-                    <div className="column is-narrow has-text-right"><img src={one} width={210} height={122}/></div>
-                    <div className="column is-narrow has-text-left"><img src={bePartOf} width={271} height={271}/></div>
+                    <div className="column is-narrow is-flex is-align-content-baseline">
+                        <p className={"is-italic has-text-info has-text-weight-bold"} style={{fontSize:"76px"}}>1.</p>
+                        <div className={"ml-4 pt-5"}>
+                            <p className={"has-text-primary has-text-weight-bold"} style={{fontSize:"18px"}}>Be part of the Local <br/> Community</p>
+                            <p className={"has-text-weight-semibold"} style={{fontSize:"14px"}}>Create a Profile and plan your Trip</p>
+                        </div>
+                    </div>
+                    <div className={"column is-1"}></div>
+                    <div className="column is-narrow has-text-left"><img src={bePartOf} width={271} height={271}/>
+                    </div>
                 </div>
                 {/* STEP 2 */}
                 <div className="columns is-vcentered is-centered">
                     <div className="column is-narrow has-text-right"><img src={findYourPref} width={271} height={271}/></div>
-                    <div className="column is-narrow has-text-left"><img src={two} width={210} height={122}/></div>
+                    <div className={"column is-1"}></div>
+                    <div className="column is-narrow is-flex is-align-content-baseline">
+                        <p className={"is-italic has-text-info has-text-weight-bold"} style={{fontSize:"76px"}}>2.</p>
+                        <div className={"ml-4 py-6"}>
+                            <p className={"has-text-primary has-text-weight-bold"} style={{fontSize:"18px"}}>Find your preferred Hosts</p>
+                            <p className={"has-text-weight-semibold"} style={{fontSize:"14px"}}>Search Hosts by location</p>
+                        </div>
+                    </div>
                 </div>
                 {/* STEP 3 */}
                 <div className="columns is-vcentered is-centered">
-                    <div className="column is-narrow has-text-right"><img src={three} width={210} height={122}/></div>
+                    <div className="column is-narrow is-flex is-align-content-baseline">
+                        <p className={"is-italic has-text-info has-text-weight-bold"} style={{fontSize:"76px"}}>3.</p>
+                        <div className={"ml-4 py-6"}>
+                            <p className={"has-text-primary has-text-weight-bold"} style={{fontSize:"18px"}}>Book your Stay</p>
+                            <p className={"has-text-weight-semibold"} style={{fontSize:"14px"}}>Let the adventure begin</p>
+                        </div>
+                    </div>
+                    <div className={"column is-1"}></div>
                     <div className="column is-narrow has-text-left"><img src={bookYourStay} width={271} height={271}/></div>
                 </div>
                 {/* STEP 4 */}
                 <div className="columns is-vcentered is-centered">
                     <div className="column is-narrow has-text-right"><img src={review} width={271} height={271}/></div>
-                    <div className="column is-narrow has-text-left"><img src={four} width={210} height={122}/></div>
+                    <div className={"column is-1"}></div>
+                    <div className="column is-narrow is-flex is-align-content-baseline">
+                        <p className={"is-italic has-text-info has-text-weight-bold"} style={{fontSize:"76px"}}>4.</p>
+                        <div className={"ml-4 py-6"}>
+                            <p className={"has-text-primary has-text-weight-bold"} style={{fontSize:"18px"}}>Review</p>
+                            <p className={"has-text-weight-semibold"} style={{fontSize:"14px"}}>Give your feedback</p>
+                        </div>
+                    </div>
                 </div>
             </React.Fragment>
         </React.Fragment>
