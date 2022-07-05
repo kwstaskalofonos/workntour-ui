2e73d8a56027994f823e3ce41caeef096d20fb73
Κwtsas kalofonos <konkalof1@gmail.com>
2022-07-05 08:55:33 +0300

    Opportunities in progress.

--- src/state/stores/opportunity/models.ts
+++ src/state/stores/opportunity/models.ts
@@ -1,5 +1,86 @@
+import {Role} from "@src/state/stores/user/models";
+
+export enum OpportunityCategory{
+     hostel,
+     hotel,
+     guestHouse,
+     farm,
+     ecoVillage,
+     skiCenter,
+     camping,
+     ngo,
+     localCommunity,
+     animalShelter,
+     surfClub,
+     winery,
+     boat,
+     homeStay,
+     individual,
+     privateProject
+}
+
+export enum TypeOfHelpNeeded{
+     reception,
+     cleaning,
+     houseKeeping,
+     administrativeTasks,
+     tourGuide,
+     babysitter,
+     vet,
+     animalCare,
+     languages,
+     sportsTeacher,
+     surfing,
+     personalTrainer,
+     farming,
+     painting,
+     handyman,
+     socialMedia,
+     photography,
+     videography,
+     webDevelopment,
+     cooking,
+     bartending,
+     service
+}
+
+export enum Accomodation{
+     privateRoom,
+     sharedRoom,
+     dorm,
+     tent
+}
+
+export enum Meal{
+     breakfast,
+     lunch,
+     dinner,
+     useSharedKitchen
+}
+
 export interface Opportunity{
      opportunityId:string,
      memberId:string,
-     role:string
+     role:Role,
+     opportunityCategory:OpportunityCategory,
+     jobTitle:string,
+     jobDescription:string,
+     typeOfHelpNeeded:TypeOfHelpNeeded[],
+     /*opportunityLocation:any,*/
+     // opportunityDates:any,
+     minimumDays:number;
+     maximumDays:number,
+     totalWorkingHours:number,
+     daysOff:number,
+     // languagesRequired:any,
+     // languagesSpoken:any,
+     accommodation:Accomodation,
+     meals:Meal[],
+     additionalOfferings:string,
+     // learningOpportunities:any,
+     adventuresOffered:string,
+     wifi:boolean,
+     smokingAllowed:boolean,
+     petsAllowed:boolean
 }
+
--- src/views/host/opportunities/AddOpportunityTab.tsx
+++ src/views/host/opportunities/AddOpportunityTab.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import {useForm} from "react-hook-form";
 
 const AddOpportunityTab:React.FunctionComponent = () =>{
+
+    const form = useForm();
 
     return(
         <React.Fragment></React.Fragment>
