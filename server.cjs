72e901cd522b7f86426504629a2362dd672da463
Κwtsas kalofonos <konkalof1@gmail.com>
2022-07-25 00:13:34 +0300

    Fixes.

--- server.cjs
+++ server.cjs
@@ -4,8 +4,10 @@ var express = require('express');
 var app = express();
 
 // app.use(express.static(path.join(__dirname, 'dist')));
-app.get('/',function(req, res){//get,put,post,delete
-    res.sendFile(__dirname + '/dist');
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 app.set('port', process.env.PORT || 8080);
 
