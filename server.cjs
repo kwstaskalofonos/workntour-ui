31f229ff15655e742fbaa41911b94f5dde8f3354
Κwtsas kalofonos <konkalof1@gmail.com>
2022-07-27 01:29:25 +0300

    Fixes.

--- server.cjs
+++ server.cjs
@@ -11,7 +11,9 @@ app.get('*', function(req, res) {
 });
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,memberId");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Credentials', true);
     next();
 });
 // app.get('/home', function(req, res) {
