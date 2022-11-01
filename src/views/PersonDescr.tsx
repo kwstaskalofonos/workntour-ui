802ec16580f81c65519e554bd40789d64881656d
Κwtsas kalofonos <konkalof1@gmail.com>
2022-11-01 07:30:03 +0200

    fixes.

--- src/views/PersonDescr.tsx
+++ src/views/PersonDescr.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import {isMobile} from "react-device-detect";
 
 interface Props {
     src: any,
@@ -11,29 +12,49 @@ const PersonDescr: React.FunctionComponent<Props> = ({src, name, position,descri
 
     const [showDesc, setShowDesc] = useState<boolean>(false);
 
-    useEffect(()=>{
-        console.log(showDesc);
-    },[showDesc])
-
     return (
         <div>
-            <div className={"is-flex is-justify-content-center" + (showDesc ? " is-hidden" : "")}>
-                <figure className="image is-128x128">
-                    <img onMouseOut={() => setShowDesc(false)}
-                         onMouseEnter={() => setShowDesc(true)}
-                        className="is-rounded" src={src}/>
-                </figure>
-            </div>
-            <div className={"is-flex is-justify-content-center" + (showDesc ? " is-hidden" : "")}>
-                <p className={"is-size-6 has-text-weight-bold"}>{name}</p>
-            </div>
-            <div className={"is-flex is-justify-content-center" + (showDesc ? " is-hidden" : "")}>
-                <p className={"is-size-7 has-text-weight-semibold has-text-primary"}>{position}</p>
-            </div>
+            {isMobile ?
+                <React.Fragment>
+                    <div className={"is-flex is-justify-content-center"}>
+                        <figure className="image is-128x128">
+                            <img onMouseOut={() => setShowDesc(false)}
+                                 onMouseEnter={() => setShowDesc(true)}
+                                 className="is-rounded" src={src}/>
+                        </figure>
+                    </div>
+                    <div className={"is-flex is-justify-content-center"}>
+                        <p className={"is-size-6 has-text-weight-bold"}>{name}</p>
+                    </div>
+                    <div className={"is-flex is-justify-content-center"}>
+                        <p className={"is-size-7 has-text-weight-semibold has-text-primary"}>{position}</p>
+                    </div>
 
-            <div className={"box has-background-primary has-text-white-bis"+ (!showDesc ? " is-hidden" : "")}>
-                {description}
-            </div>
+                    <div className={"box has-background-primary has-text-white-bis"}>
+                        {description}
+                    </div>
+                </React.Fragment>:
+                <React.Fragment>
+                    <div className={"is-flex is-justify-content-center" + (showDesc ? " is-hidden" : "")}>
+                        <figure className="image is-128x128">
+                            <img onMouseOut={() => setShowDesc(false)}
+                                 onMouseEnter={() => setShowDesc(true)}
+                                 className="is-rounded" src={src}/>
+                        </figure>
+                    </div>
+                    <div className={"is-flex is-justify-content-center" + (showDesc ? " is-hidden" : "")}>
+                        <p className={"is-size-6 has-text-weight-bold"}>{name}</p>
+                    </div>
+                    <div className={"is-flex is-justify-content-center" + (showDesc ? " is-hidden" : "")}>
+                        <p className={"is-size-7 has-text-weight-semibold has-text-primary"}>{position}</p>
+                    </div>
+
+                    <div className={"box has-background-primary has-text-white-bis"+ (!showDesc ? " is-hidden" : "")}>
+                        {description}
+                    </div>
+                </React.Fragment>
+
+            }
         </div>
 )
 };
