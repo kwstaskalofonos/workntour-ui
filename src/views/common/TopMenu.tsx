b3347d4b305514b4b0a0a7526f9f459dacdd0499
Κwtsas kalofonos <konkalof1@gmail.com>
2022-11-12 15:22:27 +0200

    disable login and sign up

--- src/views/common/TopMenu.tsx
+++ src/views/common/TopMenu.tsx
@@ -71,10 +71,10 @@ const TopMenu: React.FunctionComponent = () =>{
                         {!isAuthenticated ?
                             <div className="navbar-item">
                                 <div className="buttons">
-                                    <a className="button is-primary"
+                                    <a className="button is-primary" style={{pointerEvents:'none'}}
                                        onClick={()=>loginModalHandler.current?.open()}>Log In</a>
                                     <a className="button is-outlined"
-                                       style={{"border":"1px solid #7E6FD8","color":"#7E6FD8"}}
+                                       style={{"border":"1px solid #7E6FD8","color":"#7E6FD8",pointerEvents:'none'}}
                                        onClick={()=>registrationModalHandler.current?.open()}>Sign Up</a>
                                 </div>
                             </div>:
